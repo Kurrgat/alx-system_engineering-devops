6e0f5c2a37092de6e8276aea7d94714a0b70f4d5
Kurrgat <mrcykj@gmail.com>
2024-03-23 03:41:42 +0300

    Task 1: Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,12 @@
 # Installs puppet-lint
 
-package { 'python3-pip':
-  ensure => 'installed',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
-package { 'libpq-dev':
-  ensure => 'installed',
-}
-
-package { 'build-essential':
-  ensure => 'installed',
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip', 'libpq-dev', 'build-essential'],
+# Ensure Werkzeug is installed with a compatible version
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
